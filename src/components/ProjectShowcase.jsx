19e2c1bd5354eeeae4141c54d4ab7d81c8ffc755
ali-hamza-jutt <alihamza16jutt@outlook.com>
2024-08-05 15:21:04 +0500

    Make ProjectShowcase Component Responsive
    - Made ProjectShowcase Component responsive using media queries
    - Render 2 products on mobile screen

--- src/components/ProjectShowcase.jsx
+++ src/components/ProjectShowcase.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import specialProducts from '../data/specialProducts';
 import '../styles/ProductShowcase.css';
+import useScreenSize from '../hooks/useScreenSize';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
@@ -32,11 +33,13 @@ const MainProduct = () => {
 }
 
 const ProductGrid = ({ products }) => {
+    const {isMobile}=useScreenSize();
+    const displayedProducts=isMobile?products.slice(0,2):products;
     return (
         <div className="product-grid-container">
             <h2>Perfume Parlor Special</h2>
             <div className="product-grid">
-                {products.map((product, index) => {
+                {displayedProducts.map((product, index) => {
                     // Determine the class based on the index
                     const itemClass = index < 3 ? 'top-grid-item' : 'bottom-grid-item';
 
